983bdce069d258d8471c07606e7ba9aee1484a40
Joseph Little <joelittle11@live.com>
2024-09-26 14:58:42 +0100

    New Building 1

--- workload/terraform/jamfpro/buildings.tf
+++ workload/terraform/jamfpro/buildings.tf
@@ -1,13 +1,13 @@
 
-# resource "jamfpro_building" "jamfpro_building_001" {
-#   name            = "tf-demo-Apple-Park-jl-1"
-#   street_address1 = "The McIntosh Tree"
-#   street_address2 = "One Apple Park Way"
-#   city            = "Cupertino"
-#   state_province  = "California"
-#   zip_postal_code = "95014"
-#   country         = "The United States of America"
-# }
+resource "jamfpro_building" "jamfpro_building_001" {
+  name            = "tf-demo-Apple-Park-jl-1"
+  street_address1 = "The McIntosh Tree"
+  street_address2 = "One Apple Park Way"
+  city            = "Cupertino"
+  state_province  = "California"
+  zip_postal_code = "95014"
+  country         = "The United States of America"
+}
 
 # resource "jamfpro_building" "jamfpro_building_002" {
 #   name            = "tf-demo-jamf-headquarters"
